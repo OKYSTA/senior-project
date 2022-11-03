de7c57beabd95b262e22bd067e5d8fd9cd40cba6
OKYSTA <c0119059e1@edu.teu.ac.jp>
2022-11-04 06:06:28 +0900

    not complete

--- make_datasets.py
+++ make_datasets.py
@@ -6,5 +6,38 @@ import os
 from sklearn.cluster import KMeans
 import numpy as np
 from sklearn.decomposition import PCA
+from random import randint
 
-IMPATH_LIST = ""
+INPUTDIR_PATH = "../datasets/convert_jpg/sets/set"
+OUTPUTDIR_PATH = "../datasets/choice_jpg{}/set{}"
+SETS_NUMBER = 10
+GET_NUMBER = int(input('how many photos chice? ->'))
+
+def set_labels():
+    labels = []
+    for i in range(SETS_NUMBER):
+        for j in range(GET_NUMBER):
+            labels.append(i)
+    return labels
+
+def make_dir():
+    for i in range(SETS_NUMBER):
+        dir_name = OUTPUTDIR_PATH.format(GET_NUMBER, i)
+        if os.path.exists(dir_name):
+            shutil.rmtree(dir_name)
+        os.makedirs(dir_name)
+    
+    return 
+
+def copy_imgaes(labels):
+    for i in range(SETS_NUMBER):
+        inpath_list = glob(INPUTDIR_PATH + "{}/*".format(i))
+        for label in (labels):
+            limit = len(inpath_list) - 1
+            target = inpath_list.pop(randint(0, limit))
+            shutil.copyfile(target, OUTPUTDIR_PATH.format(GET_NUMBER, label) + "/{}".format(target.split("\\")[-1]))
+    return
+
+label = set_labels()
+make_dir()
+copy_imgaes(label)
